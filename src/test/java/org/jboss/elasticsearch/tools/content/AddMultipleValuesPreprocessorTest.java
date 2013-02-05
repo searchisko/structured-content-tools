ce37ca30955707acc10701d9a458db0299edb9ac
Vlastimil Elias <vlastimil.elias@worldonline.cz>
2013-02-05 17:14:29 +0100

    some unit tests improved

--- src/test/java/org/jboss/elasticsearch/tools/content/AddMultipleValuesPreprocessorTest.java
+++ src/test/java/org/jboss/elasticsearch/tools/content/AddMultipleValuesPreprocessorTest.java
@@ -21,78 +21,80 @@ import org.junit.Test;
  */
 public class AddMultipleValuesPreprocessorTest {
 
-  @Test
-  public void init_settingerrors() {
-    AddMultipleValuesPreprocessor tested = new AddMultipleValuesPreprocessor();
-    Map<String, Object> settings = null;
+	@Test
+	public void init_settingerrors() {
+		AddMultipleValuesPreprocessor tested = new AddMultipleValuesPreprocessor();
+		Map<String, Object> settings = null;
 
-    // case - settings mandatory
-    try {
-      tested.init("Test mapper", null, settings);
-      Assert.fail("SettingsException must be thrown");
-    } catch (SettingsException e) {
-      Assert.assertEquals("'settings' section is not defined for preprocessor Test mapper", e.getMessage());
-    }
+		// case - settings mandatory
+		try {
+			tested.init("Test mapper", null, settings);
+			Assert.fail("SettingsException must be thrown");
+		} catch (SettingsException e) {
+			Assert.assertEquals("'settings' section is not defined for preprocessor Test mapper", e.getMessage());
+		}
 
-    // case - no more mandatory setting fields
-    settings = new HashMap<String, Object>();
-    tested.init("Test mapper", null, settings);
-  }
+		// case - no more mandatory setting fields
+		settings = new HashMap<String, Object>();
+		tested.init("Test mapper", null, settings);
+	}
 
-  @Test
-  public void init() {
-    AddMultipleValuesPreprocessor tested = new AddMultipleValuesPreprocessor();
+	@Test
+	public void init() {
+		AddMultipleValuesPreprocessor tested = new AddMultipleValuesPreprocessor();
 
-    {
-      Map<String, Object> settings = new HashMap<String, Object>();
-      settings.put("field1", "source");
-      settings.put("field2", "");
+		{
+			Map<String, Object> settings = new HashMap<String, Object>();
+			settings.put("field1", "source");
+			settings.put("field2", "");
 
-      tested.init("Test mapper", null, settings);
-      Assert.assertEquals("Test mapper", tested.getName());
-      Assert.assertEquals(2, tested.fields.size());
-      Assert.assertEquals(settings, tested.fields);
-    }
-  }
+			tested.init("Test mapper", null, settings);
+			Assert.assertEquals("Test mapper", tested.getName());
+			Assert.assertEquals(2, tested.fields.size());
+			Assert.assertEquals(settings, tested.fields);
+		}
+	}
 
-  @Test
-  public void preprocessData() {
+	@Test
+	public void preprocessData() {
 
-    AddMultipleValuesPreprocessor tested = new AddMultipleValuesPreprocessor();
-    Map<String, Object> settings = new HashMap<String, Object>();
-    settings.put("field_constant", "constant value");
-    settings.put("field_empty", "");
-    settings.put("field_null", null);
-    settings.put("field_null_replace", null);
-    settings.put("field_replace_unknown", "{user.unknown}");
-    settings.put("field_replace_simple", "{title}");
-    settings.put("field_replace_nested", "{user.name}");
-    settings.put("field_replace.complex", "I'm {user.name} and like to read '{title}'");
-    tested.fields = settings;
+		AddMultipleValuesPreprocessor tested = new AddMultipleValuesPreprocessor();
+		Map<String, Object> settings = new HashMap<String, Object>();
+		settings.put("field_constant", "constant value");
+		settings.put("field_empty", "");
+		settings.put("field_null", null);
+		settings.put("field_null_replace", null);
+		settings.put("field_replace_unknown", "{user.unknown}");
+		settings.put("field_replace_simple", "{title}");
+		settings.put("field_replace_nested", "{user.name}");
+		settings.put("field_replace.complex", "I'm {user.name} and like to read '{title}'");
+		settings.put("field_replace.complex2", "{title} - {user.name}");
+		tested.fields = settings;
 
-    // case - not NPE
-    tested.preprocessData(null);
+		// case - not NPE
+		tested.preprocessData(null);
 
-    // case - leave null if no value defined
-    {
-      Map<String, Object> values = new HashMap<String, Object>();
-      values.put("title", "Gratest book ever");
-      values.put("field_null_replace", "replace me");
-      Map<String, Object> user = new HashMap<String, Object>();
-      user.put("name", "Joe");
-      values.put("user", user);
+		// case - leave null if no value defined
+		{
+			Map<String, Object> values = new HashMap<String, Object>();
+			values.put("title", "Gratest book ever");
+			values.put("field_null_replace", "replace me");
+			Map<String, Object> user = new HashMap<String, Object>();
+			user.put("name", "Joe");
+			values.put("user", user);
 
-      tested.preprocessData(values);
-      Assert.assertEquals("constant value", values.get("field_constant"));
-      Assert.assertEquals("", values.get("field_empty"));
-      Assert.assertNull(values.get("field_null"));
-      Assert.assertNull(values.get("field_null_replace"));
-      Assert.assertEquals("", values.get("field_replace_unknown"));
-      Assert.assertEquals("Gratest book ever", values.get("field_replace_simple"));
-      Assert.assertEquals("Joe", values.get("field_replace_nested"));
-      Assert.assertEquals("I'm Joe and like to read 'Gratest book ever'",
-          XContentMapValues.extractValue("field_replace.complex", values));
-    }
+			tested.preprocessData(values);
+			Assert.assertEquals("constant value", values.get("field_constant"));
+			Assert.assertEquals("", values.get("field_empty"));
+			Assert.assertNull(values.get("field_null"));
+			Assert.assertNull(values.get("field_null_replace"));
+			Assert.assertEquals("", values.get("field_replace_unknown"));
+			Assert.assertEquals("Gratest book ever", values.get("field_replace_simple"));
+			Assert.assertEquals("Joe", values.get("field_replace_nested"));
+			Assert.assertEquals("I'm Joe and like to read 'Gratest book ever'",
+					XContentMapValues.extractValue("field_replace.complex", values));
+			Assert.assertEquals("Gratest book ever - Joe", XContentMapValues.extractValue("field_replace.complex2", values));
+		}
 
-  }
+	}
 }
--- src/test/java/org/jboss/elasticsearch/tools/content/ValueUtilsTest.java
+++ src/test/java/org/jboss/elasticsearch/tools/content/ValueUtilsTest.java
@@ -20,121 +20,126 @@ import org.junit.Test;
  */
 public class ValueUtilsTest {
 
-  @Test
-  public void isEmpty() {
-    Assert.assertTrue(ValueUtils.isEmpty(null));
-    Assert.assertTrue(ValueUtils.isEmpty(""));
-    Assert.assertTrue(ValueUtils.isEmpty("     "));
-    Assert.assertTrue(ValueUtils.isEmpty(" "));
-    Assert.assertFalse(ValueUtils.isEmpty("a"));
-    Assert.assertFalse(ValueUtils.isEmpty(" a"));
-    Assert.assertFalse(ValueUtils.isEmpty("a "));
-    Assert.assertFalse(ValueUtils.isEmpty(" a "));
-  }
+	@Test
+	public void isEmpty() {
+		Assert.assertTrue(ValueUtils.isEmpty(null));
+		Assert.assertTrue(ValueUtils.isEmpty(""));
+		Assert.assertTrue(ValueUtils.isEmpty("     "));
+		Assert.assertTrue(ValueUtils.isEmpty(" "));
+		Assert.assertFalse(ValueUtils.isEmpty("a"));
+		Assert.assertFalse(ValueUtils.isEmpty(" a"));
+		Assert.assertFalse(ValueUtils.isEmpty("a "));
+		Assert.assertFalse(ValueUtils.isEmpty(" a "));
+	}
 
-  @Test
-  public void trimToNull() {
-    Assert.assertNull(ValueUtils.trimToNull(null));
-    Assert.assertNull(ValueUtils.trimToNull(""));
-    Assert.assertNull(ValueUtils.trimToNull("     "));
-    Assert.assertNull(ValueUtils.trimToNull(" "));
-    Assert.assertEquals("a", ValueUtils.trimToNull("a"));
-    Assert.assertEquals("a", ValueUtils.trimToNull(" a"));
-    Assert.assertEquals("a", ValueUtils.trimToNull("a "));
-    Assert.assertEquals("a", ValueUtils.trimToNull(" a "));
-  }
+	@Test
+	public void trimToNull() {
+		Assert.assertNull(ValueUtils.trimToNull(null));
+		Assert.assertNull(ValueUtils.trimToNull(""));
+		Assert.assertNull(ValueUtils.trimToNull("     "));
+		Assert.assertNull(ValueUtils.trimToNull(" "));
+		Assert.assertEquals("a", ValueUtils.trimToNull("a"));
+		Assert.assertEquals("a", ValueUtils.trimToNull(" a"));
+		Assert.assertEquals("a", ValueUtils.trimToNull("a "));
+		Assert.assertEquals("a", ValueUtils.trimToNull(" a "));
+	}
 
-  @Test
-  public void parseCsvString() {
-    Assert.assertNull(ValueUtils.parseCsvString(null));
-    Assert.assertNull(ValueUtils.parseCsvString(""));
-    Assert.assertNull(ValueUtils.parseCsvString("    "));
-    Assert.assertNull(ValueUtils.parseCsvString("  ,, ,   ,   "));
-    List<String> r = ValueUtils.parseCsvString(" ORG ,,UUUU, , PEM  , ,SU07  ");
-    Assert.assertEquals(4, r.size());
-    Assert.assertEquals("ORG", r.get(0));
-    Assert.assertEquals("UUUU", r.get(1));
-    Assert.assertEquals("PEM", r.get(2));
-    Assert.assertEquals("SU07", r.get(3));
-  }
+	@Test
+	public void parseCsvString() {
+		Assert.assertNull(ValueUtils.parseCsvString(null));
+		Assert.assertNull(ValueUtils.parseCsvString(""));
+		Assert.assertNull(ValueUtils.parseCsvString("    "));
+		Assert.assertNull(ValueUtils.parseCsvString("  ,, ,   ,   "));
+		List<String> r = ValueUtils.parseCsvString(" ORG ,,UUUU, , PEM  , ,SU07  ");
+		Assert.assertEquals(4, r.size());
+		Assert.assertEquals("ORG", r.get(0));
+		Assert.assertEquals("UUUU", r.get(1));
+		Assert.assertEquals("PEM", r.get(2));
+		Assert.assertEquals("SU07", r.get(3));
+	}
 
-  @Test
-  public void createCsvString() {
-    Assert.assertNull(ValueUtils.createCsvString(null));
-    List<String> c = new ArrayList<String>();
-    Assert.assertEquals("", ValueUtils.createCsvString(c));
-    c.add("ahoj");
-    Assert.assertEquals("ahoj", ValueUtils.createCsvString(c));
-    c.add("b");
-    c.add("task");
-    Assert.assertEquals("ahoj,b,task", ValueUtils.createCsvString(c));
-  }
+	@Test
+	public void createCsvString() {
+		Assert.assertNull(ValueUtils.createCsvString(null));
+		List<String> c = new ArrayList<String>();
+		Assert.assertEquals("", ValueUtils.createCsvString(c));
+		c.add("ahoj");
+		Assert.assertEquals("ahoj", ValueUtils.createCsvString(c));
+		c.add("b");
+		c.add("task");
+		Assert.assertEquals("ahoj,b,task", ValueUtils.createCsvString(c));
+	}
 
-  @Test
-  public void processStringValuePatternReplacement() {
+	@Test
+	public void processStringValuePatternReplacement() {
 
-    // case - no NPE on empty patternValue
-    Assert.assertNull(ValueUtils.processStringValuePatternReplacement(null, null, null));
-    Map<String, Object> data = new HashMap<String, Object>();
-    Assert.assertNull(ValueUtils.processStringValuePatternReplacement(null, data, null));
+		// case - no NPE on empty patternValue
+		Assert.assertNull(ValueUtils.processStringValuePatternReplacement(null, null, null));
+		Map<String, Object> data = new HashMap<String, Object>();
+		Assert.assertNull(ValueUtils.processStringValuePatternReplacement(null, data, null));
 
-    // case - empty patternValue handling
-    Assert.assertEquals("", ValueUtils.processStringValuePatternReplacement("", null, null));
-    Assert.assertEquals("", ValueUtils.processStringValuePatternReplacement("", data, null));
+		// case - empty patternValue handling
+		Assert.assertEquals("", ValueUtils.processStringValuePatternReplacement("", null, null));
+		Assert.assertEquals("", ValueUtils.processStringValuePatternReplacement("", data, null));
 
-    // case - patternValue without keys
-    Assert.assertEquals("Ahoj", ValueUtils.processStringValuePatternReplacement("Ahoj", null, null));
-    Assert.assertEquals("Ahoj", ValueUtils.processStringValuePatternReplacement("Ahoj", data, null));
+		// case - patternValue without keys
+		Assert.assertEquals("Ahoj", ValueUtils.processStringValuePatternReplacement("Ahoj", null, null));
+		Assert.assertEquals("Ahoj", ValueUtils.processStringValuePatternReplacement("Ahoj", data, null));
 
-    // case - unclosed braces
-    Assert.assertEquals("Ahoj{", ValueUtils.processStringValuePatternReplacement("Ahoj{", data, null));
-    Assert.assertEquals("Ahoj{doma", ValueUtils.processStringValuePatternReplacement("Ahoj{doma", data, null));
-    Assert.assertEquals("{Ahoj", ValueUtils.processStringValuePatternReplacement("{Ahoj", data, null));
+		// case - unclosed braces
+		Assert.assertEquals("Ahoj{", ValueUtils.processStringValuePatternReplacement("Ahoj{", data, null));
+		Assert.assertEquals("Ahoj{doma", ValueUtils.processStringValuePatternReplacement("Ahoj{doma", data, null));
+		Assert.assertEquals("{Ahoj", ValueUtils.processStringValuePatternReplacement("{Ahoj", data, null));
 
-    // case - simple one level key - not in data
-    Assert.assertEquals("Ahoj , welcome.",
-        ValueUtils.processStringValuePatternReplacement("Ahoj {name}, welcome.", null, null));
-    Assert.assertEquals("Ahoj , welcome.",
-        ValueUtils.processStringValuePatternReplacement("Ahoj {name}, welcome.", data, null));
+		// case - simple one level key - not in data
+		Assert.assertEquals("Ahoj , welcome.",
+				ValueUtils.processStringValuePatternReplacement("Ahoj {name}, welcome.", null, null));
+		Assert.assertEquals("Ahoj , welcome.",
+				ValueUtils.processStringValuePatternReplacement("Ahoj {name}, welcome.", data, null));
+		Assert.assertEquals("", ValueUtils.processStringValuePatternReplacement("{name}", data, null));
 
-    // case - simple one level key - String found in data
-    data.put("name", "Joe");
-    Assert.assertEquals("Ahoj Joe, welcome.",
-        ValueUtils.processStringValuePatternReplacement("Ahoj {name}, welcome.", data, null));
+		// case - simple one level key - String found in data
+		data.put("name", "Joe");
+		Assert.assertEquals("Ahoj Joe, welcome.",
+				ValueUtils.processStringValuePatternReplacement("Ahoj {name}, welcome.", data, null));
+		Assert.assertEquals("Joe", ValueUtils.processStringValuePatternReplacement("{name}", data, null));
 
-    // case - simple one level key, multiple keys - Nonstring found in data
-    data.put("count", new Integer(10));
-    Assert.assertEquals("Ahoj Joe, welcome 10 times.",
-        ValueUtils.processStringValuePatternReplacement("Ahoj {name}, welcome {count} times.", data, null));
+		// case - simple one level key, multiple keys - Nonstring found in data
+		data.put("count", new Integer(10));
+		Assert.assertEquals("Ahoj Joe, welcome 10 times.",
+				ValueUtils.processStringValuePatternReplacement("Ahoj {name}, welcome {count} times.", data, null));
 
-    // case - unclosed braces after key
-    Assert.assertEquals("Ahoj Joe, welcome 10 time{s.",
-        ValueUtils.processStringValuePatternReplacement("Ahoj {name}, welcome {count} time{s.", data, null));
+		// case - unclosed braces after key
+		Assert.assertEquals("Ahoj Joe, welcome 10 time{s.",
+				ValueUtils.processStringValuePatternReplacement("Ahoj {name}, welcome {count} time{s.", data, null));
 
-    // case - dot notation in key - not in data
-    Assert.assertEquals("Ahoj , welcome.",
-        ValueUtils.processStringValuePatternReplacement("Ahoj {user.name}, welcome.", null, null));
-    Assert.assertEquals("Ahoj , welcome.",
-        ValueUtils.processStringValuePatternReplacement("Ahoj {user.name}, welcome.", data, null));
+		// case - dot notation in key - not in data
+		Assert.assertEquals("Ahoj , welcome.",
+				ValueUtils.processStringValuePatternReplacement("Ahoj {user.name}, welcome.", null, null));
+		Assert.assertEquals("Ahoj , welcome.",
+				ValueUtils.processStringValuePatternReplacement("Ahoj {user.name}, welcome.", data, null));
 
-    // case - dot notation in key - found in data
-    {
-      Map<String, Object> user = new HashMap<String, Object>();
-      user.put("name", "Lena");
-      data.put("user", user);
-      Assert.assertEquals("Ahoj Lena, welcome 10 times.",
-          ValueUtils.processStringValuePatternReplacement("Ahoj {user.name}, welcome {count} times.", data, null));
-    }
+		// case - dot notation in key - found in data
+		{
+			Map<String, Object> user = new HashMap<String, Object>();
+			user.put("name", "Lena");
+			data.put("user", user);
+			Assert.assertEquals("Ahoj Lena, welcome 10 times.",
+					ValueUtils.processStringValuePatternReplacement("Ahoj {user.name}, welcome {count} times.", data, null));
 
-    // case - original value replacement mechanism tests
-    Assert
-        .assertEquals("Ahoj Pool, welcome Pool times.", ValueUtils.processStringValuePatternReplacement(
-            "Ahoj {__original}, welcome {__original} times.", null, "Pool"));
-    data.put("count", new Integer(10));
-    Assert.assertEquals("Ahoj Pool, welcome 10 times.",
-        ValueUtils.processStringValuePatternReplacement("Ahoj {__original}, welcome {count} times.", data, "Pool"));
-    Assert.assertEquals("Ahoj , welcome 10 times.",
-        ValueUtils.processStringValuePatternReplacement("Ahoj {__original}, welcome {count} times.", data, null));
-  }
+			Assert.assertEquals("10 - Lena",
+					ValueUtils.processStringValuePatternReplacement("{count} - {user.name}", data, null));
+		}
+
+		// case - original value replacement mechanism tests
+		Assert
+				.assertEquals("Ahoj Pool, welcome Pool times.", ValueUtils.processStringValuePatternReplacement(
+						"Ahoj {__original}, welcome {__original} times.", null, "Pool"));
+		data.put("count", new Integer(10));
+		Assert.assertEquals("Ahoj Pool, welcome 10 times.",
+				ValueUtils.processStringValuePatternReplacement("Ahoj {__original}, welcome {count} times.", data, "Pool"));
+		Assert.assertEquals("Ahoj , welcome 10 times.",
+				ValueUtils.processStringValuePatternReplacement("Ahoj {__original}, welcome {count} times.", data, null));
+	}
 
 }
